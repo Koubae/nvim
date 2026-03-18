ff53a1d8776178bb96fe57dbeb76a50dd974f806
koubae <58447627+Koubae@users.noreply.github.com>
2026-03-18 18:13:16 +0100

    go prev split

--- lua/plugins/navigation-keys.lua
+++ lua/plugins/navigation-keys.lua
@@ -12,6 +12,6 @@ return {
 		{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>", desc = "Go Down Split" },
 		{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>", desc = "Go Up Slipt" },
 		{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>", desc = "Go Right Slipt" },
-		{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>", desc = "Go Previous Slipt" },
+		{ "<c-BS>", "<cmd><C-U>TmuxNavigatePrevious<cr>", desc = "Go Previous Slipt" },
 	},
 }
