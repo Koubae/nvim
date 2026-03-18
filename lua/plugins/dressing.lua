0ba2240bd173f07d946a718ce792c9f533b3642b
koubae <58447627+Koubae@users.noreply.github.com>
2026-03-18 14:36:29 +0100

    dressing 2

--- lua/plugins/dressing.lua
+++ lua/plugins/dressing.lua
@@ -1,6 +1,12 @@
 return {
-  'stevearc/dressing.nvim',
-  opts = {},
+  "folke/snacks.nvim",
+  ---@type snacks.Config
+  opts = {
+    input = {
+      -- your input configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  }
 }
-
 
