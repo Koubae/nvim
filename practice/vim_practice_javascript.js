34d770682250feeaf5e4d61d85f12465ffc96ac1
koubae <58447627+Koubae@users.noreply.github.com>
2026-03-19 16:35:35 +0100

    Practice javascript

---
+++ practice/vim_practice_javascript.js
@@ -0,0 +1,45 @@
+
+/*
+ * Press "%" to go matching parenthesis 
+ *
+ * */
+function matchParenthesisPractice() {
+
+  return function() {
+
+    return function() {
+
+      return function() {
+
+        return function() {
+
+          return {
+            someFuckedUpObject: {
+              nestedAF: {
+                Ops: {
+                  IHateMyLife: {
+                    Why: [
+                      {
+                        wtf: {
+                          key: [
+                            1, 2, 3, 4, [
+                              1, 2, 3, 4
+                            ]
+                          ]
+                        }
+                      }
+
+                    ]
+                  }
+                }
+              }
+            }
+          }
+
+        }
+      }
+    }
+  }
+
+
+} 
