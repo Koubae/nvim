34df8beb710f99f065b8975ca8252ea26699d606
koubae <58447627+Koubae@users.noreply.github.com>
2026-03-25 11:40:51 +0100

    GO: golines wraps lines on save 80

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -10,12 +10,20 @@ return {
       -- This runs goimports (which organizes imports)
       -- followed by gofumpt or lsp formatting
       go = {
+        "golines",
         "goimports",
         "gofumpt",
 
+        lsp_format = "last",
         -- NOTE:    If you have multiple formatters but only want to run
         --          the first one that is found on your system (to avoid redundant work), use the stop_after_first
         -- , stop_after_first = true
+      },
+    },
+
+    formatters = {
+      golines = {
+        prepend_args = { "--max-len=80" }, -- Your wrapping limit
       },
     },
   },
